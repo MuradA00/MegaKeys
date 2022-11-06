13adc847ef2447110c6b4a51ede77ea42a973072
Murad <abishlimurad@gmail.com>
2022-11-06 17:26:08 +0200

    main 99%

--- source/js/common.js
+++ source/js/common.js
@@ -1,1 +1,23 @@
 AOS.init();
+
+const termsTrigger = document.querySelector('.terms-trigger'),
+      policyTrigger = document.querySelector('.policy-trigger'),
+      termsModal = document.querySelector('#terms-modal'),
+      policyModal = document.querySelector('#policy-modal'),
+      closeTermsModal = document.querySelector('.close-terms'),
+      closePolicyModal = document.querySelector('.close-policy');
+
+function openModal(currentModal, closeModal) {
+  currentModal.classList.add('--show-modal');
+  closeModal.addEventListener('click', () => {
+    currentModal.classList.remove('--show-modal');
+  })
+}
+
+termsTrigger.addEventListener('click', () => {
+  openModal(termsModal, closeTermsModal);
+})
+
+policyTrigger.addEventListener('click', () => {
+  openModal(policyModal, closePolicyModal);
+})
