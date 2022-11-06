60fda8b49cf4f027dd97739195ff01c0b3a1d8c8
Murad <abishlimurad@gmail.com>
2022-11-06 23:36:46 +0200

    100%

--- source/js/common.js
+++ source/js/common.js
@@ -5,7 +5,10 @@ const termsTrigger = document.querySelector('.terms-trigger'),
       termsModal = document.querySelector('#terms-modal'),
       policyModal = document.querySelector('#policy-modal'),
       closeTermsModal = document.querySelector('.close-terms'),
-      closePolicyModal = document.querySelector('.close-policy');
+      closePolicyModal = document.querySelector('.close-policy'),
+      burger = document.querySelector('.burger')
+      mobMenu = document.querySelector('.header__nav'),
+      body = document.body;
 
 function openModal(currentModal, closeModal) {
   currentModal.classList.add('--show-modal');
@@ -20,4 +23,15 @@ termsTrigger.addEventListener('click', () => {
 
 policyTrigger.addEventListener('click', () => {
   openModal(policyModal, closePolicyModal);
+})
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('--active-burger')
+  if (burger.classList.contains('--active-burger')) {
+    mobMenu.classList.add('--active-menu')
+    body.classList.add('--body-locked')
+  } else {
+    mobMenu.classList.remove('--active-menu')
+    body.classList.remove('--body-locked')
+  }
 })
